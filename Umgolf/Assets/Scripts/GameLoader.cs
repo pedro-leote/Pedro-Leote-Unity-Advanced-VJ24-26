4fa1ce4957d2a34067b7f8614ce1ff4feff1e4bc
pedro-leote <pedro_leote@eticalgarve.com>
2025-05-18 12:33:59 +0100

    Commenting GameLoader code for help

--- Umgolf/Assets/Scripts/GameLoader.cs
+++ Umgolf/Assets/Scripts/GameLoader.cs
@@ -10,11 +10,12 @@ using UnityEngine.SceneManagement;
 //Used in the Loading screen to initialize everything into who needs it: levels, save file, next 2 scenes.
 public class GameLoader : MonoBehaviour
 {
-
+    //Bools de verificação se todos os processos funcionaram.
     [SerializeField] private bool _hasFinishedSaveLoad = false;
     [SerializeField] private bool _hasFinishedLevels = false;
     [SerializeField] private bool _hasFinishedScenes = false;
     
+    //Eventos para lançar informação obtida a quem precisar (ex.: LevelManager)
     public UnityEvent<LevelLayout[]> OnLevelsLoadedEvent = new UnityEvent<LevelLayout[]>();
     public UnityEvent OnScenesLoadedEvent = new UnityEvent();
     
@@ -23,7 +24,7 @@ public class GameLoader : MonoBehaviour
         StartCoroutine(WaitUntilStartingLoading());
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (_hasFinishedLevels && _hasFinishedScenes && _hasFinishedSaveLoad)
@@ -44,28 +45,34 @@ public class GameLoader : MonoBehaviour
         
         StopCoroutine(WaitUntilStartingLoading());
     }
+    
+    //No LoadLevels obtenho os SO's presentes no Resources folder, dou parse para um array dando-lhes load, e envio para o Dictionary do LevelManager
     private IEnumerator LoadLevels()
     {
-        LevelLayout[] foundLayouts = Resources.FindObjectsOfTypeAll<LevelLayout>();
+        LevelLayout[] foundLayouts = Resources.FindObjectsOfTypeAll<LevelLayout>(); //Obtemos os SOs dentro do Resources folder, sem subfolders.
         if (foundLayouts == null || foundLayouts.Length == 0)
         {
-            yield break;
+            yield break; // Se não houver nada, não posso fazer nada lol
         }
         
+        //Damos init a um array com o tamanho de todos os níveis encontrados.
         LevelLayout[] layoutArray = new LevelLayout[foundLayouts.Length];
         for (int i = 0; i < foundLayouts.Length; ++i)
         {
+            //Fazemos request de load a cada um destes SO's para não esperar later no jogo.
             ResourceRequest request = Resources.LoadAsync<LevelLayout>(foundLayouts[i].name);
-            yield return request; 
-            layoutArray[i] = request.asset as LevelLayout;
+            yield return request; //Esperamos até o load terminar.
+            layoutArray[i] = request.asset as LevelLayout; //E metemos como LevelLayout.
         }
+        //Anteriormente este código dava IndexOutOfRangeException, mas agora parece funcionar bem.
         
-        OnLevelsLoadedEvent?.Invoke(layoutArray);
+        OnLevelsLoadedEvent?.Invoke(layoutArray); // O problema situa-se neste evento, pois o listener é o LevelManager que depois 
+                                                  // transforma o array obtido em entries do seu SerializableDictionary. Mas não tem funcionado.
         yield return true;
         _hasFinishedLevels = true;
     }
     
-
+    //No LoadSave, procuro o .json e chamo o LoadGameState do SaveManager, obtendo a info necessária deste documento.
     private IEnumerator LoadSave()
     {
 
@@ -77,9 +84,10 @@ public class GameLoader : MonoBehaviour
         _hasFinishedSaveLoad = true;
     }
     
+    //No LoadScenes chamo o SceneLoadManager para dar async load das únicas 2 scenes que preciso.
     private IEnumerator LoadScenes()
     {
-        yield return SceneLoadManager.Instance.LoadScenes();
+        yield return SceneLoadManager.Instance.LoadScenes(); //Aqui o loading async das scenes, pelo menos no Editor, nunca parece acabar. Estranho
         OnScenesLoadedEvent?.Invoke();
         _hasFinishedScenes = true;
     }
